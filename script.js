47718584d02942982e98403e1d403cd0c7400016
Debalina Laha <32841776+Debalina99@users.noreply.github.com>
2025-05-27 17:19:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,34 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
-const subtractives = [
+  	const numeralMap = [
+    ['M', 1000],
     ['CM', 900],
+    ['D', 500],
     ['CD', 400],
+    ['C', 100],
     ['XC', 90],
+    ['L', 50],
     ['XL', 40],
+    ['X', 10],
     ['IX', 9],
-    ['IV', 4]
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
   ];
 
   let result = '';
 
- 
-  for (let [symbol, value] of subtractives) {
+  for (let [symbol, value] of numeralMap) {
     while (num >= value) {
       result += symbol;
       num -= value;
     }
   }
- 
-  const values = Object.values(obj);
-  for (let [symbol, value] of values) {
-    while (num >= value) {
-      result += symbol;
-      num -= value;
-    }
-  }
+
   return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(convertToRoman(36));
+console.log(convertToRoman(14));
 
 
 
