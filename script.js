80bd522ccfa6b80c6ea6a56859cfd8196dae55e3
Debalina Laha <32841776+Debalina99@users.noreply.github.com>
2025-05-27 16:47:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,13 +8,34 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
+const subtractives = {
+    900: 'CM', 400: 'CD',
+    90: 'XC', 40: 'XL',
+    9: 'IX', 4: 'IV'
+  };
 
-  //your code here
+  let result = '';
+  for (let key in subtractives) {
+    key = Number(key);
+    while (num >= key) {
+      result += subtractives[key];
+      num -= key;
+    }
+  }
 
+  const values = Object.values(obj);
+  for (let [symbol, value] of values) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
+  }
+
+  return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
